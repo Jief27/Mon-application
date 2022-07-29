0520ad3149eb321c238db4b014989fd9e72a1537
Jief27 <jfdarwalls2.7@gmail.com>
2022-07-29 23:28:55 +0000

    first commit

---
+++ Function.js
@@ -0,0 +1,11 @@
+function adresse(ville, pays, quartier) {
+    let ville = props.ville;
+    let pays = props.pays;
+    let quartier = props.quartier;
+    return (
+        <personne ville={ } pays={ } quartier={ } />)
+}
+
+function personne(props) {
+    return (props.ville, props.pays, props.quartier)
+}
